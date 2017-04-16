009364f049d37f3893e84425541f37f34da6761b
ZhipengShen <szpssky@gmail.com>
2017-04-16 15:09:04 +0800

    update

--- not_mnist_cnn/Pretreatment.py
+++ not_mnist_cnn/Pretreatment.py
@@ -2,6 +2,7 @@
 import random
 import numpy as np
 import os
+import sys
 from scipy import ndimage
 import pickle
 
--- not_mnist_cnn/cnn.py
+++ not_mnist_cnn/cnn.py
@@ -2,11 +2,9 @@ import pickle
 import tensorflow as tf
 import numpy as np
 
-
-def accuracy(predictions, labels):
-    return (100.0 * np.sum(np.argmax(predictions, axis=1) == np.argmax(labels, axis=1))
-            / predictions.shape[0])
-
+import random
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('./tmp', one_hot=True)
 
 with open('notMNIST.pickle', 'rb') as f:
     save = pickle.load(f)
@@ -15,7 +13,9 @@ with open('notMNIST.pickle', 'rb') as f:
     test_dataset = save['test_dataset']
     test_labels = save['test_labels']
 
-batch_size = 1
+
+
+batch_size = 16
 num_labels = 10
 num_channnels = 1
 depth = 32
@@ -24,6 +24,9 @@ path_size = 5
 
 train_dataset = np.reshape(train_dataset, (-1, image_size, image_size, num_channnels))
 train_labels = (np.arange(num_labels) == train_labels[:, None]).astype(np.float32)
+print(test_dataset.shape)
+a=test_dataset = test_dataset[0:1000]
+test_labels = test_labels[0:1000]
 
 test_dataset = np.reshape(test_dataset, (-1, image_size, image_size, num_channnels))
 test_labels = (np.arange(num_labels) == test_labels[:, None]).astype(np.float32)
@@ -38,21 +41,25 @@ with tf.name_scope('labels'):
 
 tf_test_dataset = tf.constant(test_dataset)
 
+def accuracy(predictions, labels):
+    return (100.0 * np.sum(np.argmax(predictions, axis=1) == np.argmax(labels, axis=1))
+            / predictions.shape[0])
+
 
 def cnn(data):
     with tf.name_scope('weights'):
-        weights = {'w_conv1': tf.Variable(tf.random_normal([path_size, path_size, num_channnels, depth])),
-                   'w_conv2': tf.Variable(tf.random_normal([path_size, path_size, depth, 64])),
-                   'w_fc1': tf.Variable(tf.random_normal([7 * 7 * 64, 1024])),
-                   'w_fc2': tf.Variable(tf.random_normal([1024, 1024])),
-                   'out': tf.Variable(tf.random_normal([1024, num_labels]))}
+        weights = {'w_conv1': tf.Variable(tf.random_normal([path_size, path_size, num_channnels, depth],stddev=0.1)),
+                   'w_conv2': tf.Variable(tf.random_normal([path_size, path_size, depth, 64],stddev=0.1)),
+                   'w_fc1': tf.Variable(tf.random_normal([7 * 7 * 64, 1024],stddev=0.1)),
+                   'w_fc2': tf.Variable(tf.random_normal([1024, 1024],stddev=0.1)),
+                   'out': tf.Variable(tf.random_normal([1024, num_labels],stddev=0.1))}
     with tf.name_scope('biases'):
         biases = {
-            'b_conv1': tf.Variable(tf.random_normal([32])),
-            'b_conv2': tf.Variable(tf.random_normal([64])),
-            'b_fc1': tf.Variable(tf.random_normal([1024])),
-            'b_fc2': tf.Variable(tf.random_normal([1024])),
-            'b_out': tf.Variable(tf.random_normal([num_labels]))
+            'b_conv1': tf.Variable(tf.constant(1.0,shape=[32])),
+            'b_conv2': tf.Variable(tf.constant(1.0,shape=[64])),
+            'b_fc1': tf.Variable(tf.constant(1.0,shape=[1024])),
+            'b_fc2': tf.Variable(tf.constant(1.0,shape=[1024])),
+            'b_out': tf.Variable(tf.constant(1.0,shape=[num_labels]))
         }
 
     with tf.name_scope('conv1'):
@@ -88,7 +95,7 @@ def cnn(data):
     # print("fc2", fc_2.shape)
 
     with tf.name_scope('output'):
-        output = tf.matmul(fc_2, weights['out']) + biases['b_out']
+        output = tf.matmul(fc_1, weights['out']) + biases['b_out']
 
     return output
 
@@ -96,7 +103,7 @@ def cnn(data):
 logits = cnn(tf_train_dataset)
 predict = tf.nn.softmax(logits)
 
-test_prediction = tf.nn.softmax(cnn(tf_test_dataset))
+test_prediction = tf.nn.softmax(cnn(mnist.test.images))
 
 # print(tf_train_dataset.shape)
 # print(tf_train_labels.shape)
@@ -127,7 +134,7 @@ def train():
             print("Reading model parameters from %s" % ckpt.model_checkpoint_path)
             saver.restore(sess, ckpt.model_checkpoint_path)
         else:
-            print("Created model with fresh parameters.")
+            print("parameters initializer .")
             sess.run(tf.global_variables_initializer())
 
 
@@ -136,25 +143,33 @@ def train():
 
         epoch = 0
         while True:
-            offset = (epoch * batch_size) % (train_labels.shape[0] - batch_size)
-
-            batch_data = train_dataset[offset:(offset + batch_size), :, :, :]
-            batch_labels = train_labels[offset:(offset + batch_size), :]
-
-            _, l, prediction, summary = sess.run([train_step, loss, predict, merged],
-                                                 feed_dict={tf_train_dataset: batch_data,
-                                                            tf_train_labels: batch_labels})
-            train_writer.add_summary(summary, epoch)
-
-            epoch += 1
-            if epoch % 1000 == 0:
-                print("save variables")
-                saver.save(sess, 'variables/poetry.module', global_step=epoch)
-
-            if epoch % 5000==0:
-                print("Epoch:",epoch)
-                print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
-                print("===================================================")
+            for i in range(int(mnist.train.num_examples / batch_size)):
+                x, y = mnist.train.next_batch(batch_size)
+                _, l, prediction, summary = sess.run([train_step, loss, predict, merged],feed_dict={tf_train_dataset: x,
+                                                                                                     tf_train_labels: y})
+                print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), mnist.test.labels))
+            # offset = (epoch * batch_size) % (train_labels.shape[0] - batch_size)
+            #
+            # batch_data = train_dataset[offset:(offset + batch_size), :, :, :]
+            # batch_labels = train_labels[offset:(offset + batch_size), :]
+            #
+            # _, l, prediction, summary = sess.run([train_step, loss, predict, merged],
+            #                                      feed_dict={tf_train_dataset: batch_data,
+            #                                                 tf_train_labels: batch_labels})
+            # train_writer.add_summary(summary, epoch)
+            #
+            # epoch += 1
+            # if epoch % 1000 == 0:
+            #     print("save variables")
+            #     saver.save(sess, 'variables/poetry.module', global_step=epoch)
+            #
+            # if epoch % 50==0:
+            #     print("Epoch:",epoch)
+            #     print("loss:",l)
+            #     # print('Batch Accuracy: %.1f%%' % accuracy(prediction,batch_labels))
+            #     print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
+            #
+            #     print("===================================================")
 
 
 
