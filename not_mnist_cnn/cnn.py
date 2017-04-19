4d02b3efff47b91b130e5f90725e691dc1290cc3
ZhipengShen <szpssky@gmail.com>
2017-04-19 10:36:02 +0800

    add l2 regularization

--- not_mnist_cnn/cnn.py
+++ not_mnist_cnn/cnn.py
@@ -33,29 +33,31 @@ with tf.name_scope('input_data'):
 with tf.name_scope('labels'):
     tf_train_labels = tf.placeholder(tf.float32, shape=(None, num_labels))
 
-global_step = tf.Variable(0,trainable=False)
+global_step = tf.Variable(0, trainable=False)
+
 
 def accuracy(predictions, labels):
     return (100.0 * np.sum(np.argmax(predictions, axis=1) == np.argmax(labels, axis=1))
             / predictions.shape[0])
 
 
-def cnn(data):
-    with tf.name_scope('weights'):
-        weights = {'w_conv1': tf.Variable(tf.random_normal([path_size, path_size, num_channnels, depth], stddev=0.1)),
-                   'w_conv2': tf.Variable(tf.random_normal([path_size, path_size, depth, 64], stddev=0.1)),
-                   'w_fc1': tf.Variable(tf.random_normal([7 * 7 * 64, 1024], stddev=0.1)),
-                   'w_fc2': tf.Variable(tf.random_normal([1024, 1024], stddev=0.1)),
-                   'out': tf.Variable(tf.random_normal([1024, num_labels], stddev=0.1))}
-    with tf.name_scope('biases'):
-        biases = {
-            'b_conv1': tf.Variable(tf.constant(1.0, shape=[32])),
-            'b_conv2': tf.Variable(tf.constant(1.0, shape=[64])),
-            'b_fc1': tf.Variable(tf.constant(1.0, shape=[1024])),
-            'b_fc2': tf.Variable(tf.constant(1.0, shape=[1024])),
-            'b_out': tf.Variable(tf.constant(1.0, shape=[num_labels]))
-        }
+with tf.name_scope('weights'):
+    weights = {'w_conv1': tf.Variable(tf.random_normal([path_size, path_size, num_channnels, depth], stddev=0.1)),
+               'w_conv2': tf.Variable(tf.random_normal([path_size, path_size, depth, 64], stddev=0.1)),
+               'w_fc1': tf.Variable(tf.random_normal([7 * 7 * 64, 1024], stddev=0.1)),
+               'w_fc2': tf.Variable(tf.random_normal([1024, 1024], stddev=0.1)),
+               'out': tf.Variable(tf.random_normal([1024, num_labels], stddev=0.1))}
+with tf.name_scope('biases'):
+    biases = {
+        'b_conv1': tf.Variable(tf.constant(1.0, shape=[32])),
+        'b_conv2': tf.Variable(tf.constant(1.0, shape=[64])),
+        'b_fc1': tf.Variable(tf.constant(1.0, shape=[1024])),
+        'b_fc2': tf.Variable(tf.constant(1.0, shape=[1024])),
+        'b_out': tf.Variable(tf.constant(1.0, shape=[num_labels]))
+    }
 
+
+def cnn(data):
     with tf.name_scope('conv1'):
         conv1 = tf.nn.relu(
             tf.nn.conv2d(data, weights['w_conv1'], [1, 1, 1, 1], padding="SAME", use_cudnn_on_gpu=False, name="conv1") + \
@@ -85,12 +87,11 @@ def cnn(data):
 
     with tf.name_scope('fc_2'):
         fc_2 = tf.nn.relu(tf.matmul(fc_1, weights['w_fc2']) + biases['b_fc2'])
-        fc_2_dropout = tf.nn.dropout(fc_2,0.9)
 
     # print("fc2", fc_2.shape)
 
     with tf.name_scope('output'):
-        output = tf.matmul(fc_2_dropout, weights['out']) + biases['b_out']
+        output = tf.matmul(fc_2, weights['out']) + biases['b_out']
 
     return output
 
@@ -98,19 +99,20 @@ def cnn(data):
 logits = cnn(tf_train_dataset)
 predict = tf.nn.softmax(logits)
 
-
 with tf.name_scope('loss'):
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf_train_labels, logits=logits))
+    loss = loss + 0.01 * (
+        tf.nn.l2_loss(weights['w_fc1']) + tf.nn.l2_loss(weights['w_fc2']) + tf.nn.l2_loss(
+            biases['b_fc1'] + tf.nn.l2_loss(biases['b_fc2'])) + tf.nn.l2_loss(weights['out']) + tf.nn.l2_loss(
+            biases['b_out']))
+
     tf.summary.scalar('loss', loss)
 
 with tf.name_scope('training'):
-    starter_learning_rate =0.05
-    learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,1000, 0.96, staircase=True)
-    train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss,global_step=global_step)
-    tf.summary.scalar('learning rate',learning_rate)
-
-
-tf.summary.scalar('global step',global_step)
+    starter_learning_rate = 0.05
+    learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step, 1000, 0.96, staircase=True)
+    train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss, global_step=global_step)
+    tf.summary.scalar('learning rate', learning_rate)
 
 
 # tvars = tf.trainable_variables()
